26189b612e36ea0746189d55ed8c5500f5618b2b
Truls Edvard Stokke <trulses@gmail.com>
2011-12-25 09:49:36 +0100

    Made comparison and equality functions more sane, now you may compare any object with any other object and get the same result for the comparison every time as long as they are not mutated for the scope of their existence.

--- lang/metaclass/metaclass.c
+++ lang/metaclass/metaclass.c
@@ -1,7 +1,26 @@
+#include <string.h>
 #include <coal/implementation.h>
 
 int lang(cmp) (const var self, const var other) {
-  ClassCallTemplate(cmp, self, other);
+  const class(metaclass) * s_class, *o_class;
+
+  /* identically the same */
+  if (self == other)
+    return 0;
+
+  s_class = lang(getClass)(self);
+  o_class = lang(getClass)(other);
+
+  /* self instance of other's class? */
+  if (lib(instanceof)(self, o_class))
+    return o_class->cmp(self, other);
+  /* other instance of self's class? */
+  else if (lib(instanceof)(other, s_class))
+    return s_class->cmp(self, other);
+  /* completely different types,
+     we compare the class names. */
+  else
+    return strcmp(s_class->name, o_class->name);
 }
 
 var lang(constructor) (var self, va_list * app) {
@@ -13,7 +32,25 @@ var lang(destructor) (var self) {
 }
 
 bool lang(equals) (const var self, const var other) {
-  ClassCallTemplate(equals, self, other);
+  const class(metaclass) * s_class, o_class;
+
+  /* identically the same */
+  if (self == other)
+    return true;
+
+  s_class = lang(getClass)(self);
+  o_class = lang(getClass)(other);
+
+  /* self instance of other's class */
+  if (lib(instanceof)(self, o_class))
+    return o_class->cmp(self, other);
+  /* other instance of self's class */
+  else if (lib(instanceof)(other, s_class))
+    return s_class->cmp(self, other);
+  /* completely different types,
+     can't be equal */
+  else
+    return false;
 }
 
 const var lang(getClass) (const var _self) {
