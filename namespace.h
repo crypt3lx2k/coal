8bb88b76cbfbffde03ec2cf6e099f8e8454bb06d
Truls Edvard Stokke <trulses@gmail.com>
2012-01-04 17:51:41 +0100

    Forgot to delete the namespace files with git rm instead of rm.

--- namespace.h
+++
@@ -1,9 +0,0 @@
-#ifndef COAL_NAMESPACE_H__
-#define COAL_NAMESPACE_H__
-
-#include <coal/core/namespaces.h>
-
-#define lib(identifier) \
-  namespace(coal, identifier)
-
-#endif /* COAL_NAMESPACE_H__ */
