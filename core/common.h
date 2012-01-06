dfefd5183011bb494014c1a8e202489a1f8af36d
Truls Edvard Stokke <trulses@gmail.com>
2012-01-06 14:47:56 +0100

    Fixed a last minute change that made common.h invalid.

--- core/common.h
+++ core/common.h
@@ -1,11 +1,11 @@
 #ifndef COAL_CORE_COMMON_H__
 #define COAL_CORE_COMMON_H__
 
+#include <pthread.h>
+
 #ifndef COAL_CORE_IMPLEMENTATION_H__
 # error "Never include <coal/core/common.h> directly; use <coal/core/implementation.h> instead."
 #endif
-
-#include <coal/core/threading.h>
 
 /**
  * Sets up a class description.
