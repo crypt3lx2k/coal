3e936cc8213f5fd93a10973294bcc3619f84ce84
Truls Edvard Stokke <trulses@gmail.com>
2012-01-06 17:42:17 +0100

    Added a utility_mutex, which currently is only a thin layer over the pthread_mutex.

--- core/common.h
+++ core/common.h
@@ -1,7 +1,7 @@
 #ifndef COAL_CORE_COMMON_H__
 #define COAL_CORE_COMMON_H__
 
-#include <pthread.h>
+#include <coal/core/utility_mutex.h>
 
 #ifndef COAL_CORE_IMPLEMENTATION_H__
 # error "Never include <coal/core/common.h> directly; use <coal/core/implementation.h> instead."
@@ -12,14 +12,14 @@
  */
 #define SETUP_CLASS_DESCRIPTION(name, ...)      \
   static const var name##__ = NULL;             \
-  static pthread_mutex_t name##_##mutex##__ =   \
-    PTHREAD_MUTEX_INITIALIZER;                  \
+  static utility_mutex name##_##mutex##__ =     \
+    utility_mutex_open();                       \
                                                 \
   const var name (void) {                       \
-    pthread_mutex_lock(&name##_##mutex##__);    \
+    utility_mutex_lock(&name##_##mutex##__);    \
     if (name##__ == NULL)                       \
       name##__ = coal_new(__VA_ARGS__);         \
-    pthread_mutex_unlock(&name##_##mutex##__);  \
+    utility_mutex_unlock(&name##_##mutex##__);  \
                                                 \
     return name##__;                            \
   }
---
+++ core/utility_mutex.h
@@ -0,0 +1,54 @@
+#ifndef COAL_CORE_UTILITY_MUTEX_H__
+#define COAL_CORE_UTILITY_MUTEX_H__
+
+/*
+ * The following macros are only a thin layer over the
+ * pthread_mutex functions. The reason these exist is to ease
+ * any possible future transition away from the pthread
+ * library to something else, or to add functionality later
+ * like better error checking or deadlock detection.
+ *
+ * Will likely add functionality on an need to implement
+ * basis.
+ */
+
+#include <pthread.h>
+
+/**
+ * utility_mutex
+ * Mutual exclusion lock.
+ */
+typedef pthread_mutex_t utility_mutex;
+
+/**
+ * utility_mutex_open
+ * Provides a valid constant open
+ * utility_mutex.
+ *
+ * @return (utility_mutex) open utility_mutex
+ */
+#define utility_mutex_open() PTHREAD_MUTEX_INITIALIZER
+
+/**
+ * utility_mutex_lock
+ * Attempts to acquire the mutex,
+ * will block until the thread is
+ * successful.
+ *
+ * @param  (utility_mutex *) the mutex in question
+ * @return (int) zero on success and non-zero otherwise
+ */
+#define utility_mutex_lock(mutex) \
+  pthread_mutex_lock(mutex)
+
+/**
+ * utility_mutex_unlock
+ * Attempts to unlock the mutex.
+ *
+ * @param  (utility_mutex *) the mutex in question
+ * @return (int) zero on success and non-zero otherwise
+ */
+#define utility_mutex_unlock(mutex) \
+  pthread_mutex_unlock(mutex)
+
+#endif /* COAL_CORE_UTILITY_MUTEX_H__ */
