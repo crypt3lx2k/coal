fb76357e9f2e734ca21463153b1969d2220c281f
Truls Edvard Stokke <trulses@gmail.com>
2012-01-06 12:11:30 +0100

    Synchronized the generation of global class descriptions with mutexes.

--- core/object_orientation.h
+++ core/object_orientation.h
@@ -1,6 +1,9 @@
 #ifndef COAL_OBJECT_ORIENTATION_H__
 #define COAL_OBJECT_ORIENTATION_H__
 
+/* for SETUP_CLASS_DESCRIPTION */
+#include <pthread.h>
+
 #ifndef COAL_CORE_IMPLEMENTATION_H__
 # error "Never include <coal/core/object_orientation.h> directly; use <coal/core/implementation.h> instead."
 #endif
@@ -22,10 +25,16 @@
  */
 #define SETUP_CLASS_DESCRIPTION(name, ...)	\
   static const var name##__ = NULL;		\
+  static pthread_mutex_t name##_##mutex##__ =	\
+    PTHREAD_MUTEX_INITIALIZER;			\
 						\
   const var name (void) {			\
-    return name##__ != NULL ? name##__ :	\
-      (name##__ = coal_new(__VA_ARGS__));	\
+    pthread_mutex_lock(&name##_##mutex##__);	\
+    if (name##__ == NULL)			\
+      name##__ = coal_new(__VA_ARGS__);		\
+    pthread_mutex_unlock(&name##_##mutex##__);	\
+						\
+    return name##__;				\
   }
 
 #endif /* COAL_OBJECT_ORIENTATION_H__ */
