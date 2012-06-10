df1c8a86d817a3ee4529f33943275f6366608814
Truls Edvard Stokke <trulses@gmail.com>
2012-06-11 00:20:55 +0200

    Modified SETUP_CLASS_DESCRIPTION for the better, now it never does unnecessary or unused allocation while still being lock-free. I'm afraid it won't work on all systems, all compatible compilers or all optimization levels, ready to revert if something goes bad.

--- private/object_orientation.h
+++ private/object_orientation.h
@@ -20,7 +20,6 @@
 #ifndef COAL_PRIVATE_OBJECT_ORIENTATION_H
 #define COAL_PRIVATE_OBJECT_ORIENTATION_H
 
-#include <stdlib.h> /* free */
 #include <coal/private/atomic.h>
 
 /* designates the start of a class declaration. A class
@@ -37,30 +36,28 @@
 /**
  * Sets up a class description.
  */
-#define SETUP_CLASS_DESCRIPTION(name, ...)       \
-  static val name##__ = NULL;                    \
-                                                 \
-  val name (void) {                              \
-    var _desc = NULL;                            \
-                                                 \
-    if (name##__ == NULL) {                      \
-      _desc = coal_new(__VA_ARGS__);             \
-      if (!atomic_cas(&name##__, NULL, _desc)) { \
-        /* someone else succeeded in swapping    \
-           the value of the class description,   \
-           we must attempt to deallocate */      \
-        /* in case this is a metaclass with      \
-           an actual destructor */               \
-        coal_base_Object_destructor(_desc);      \
-        /* in case it isn't (this is likely)     \
-           we free the pointer returned from     \
-           new directly, since we can't trust    \
-           the return value of the destructor */ \
-        free(_desc);                             \
-      }                                          \
-    }                                            \
-                                                 \
-    return name##__;                             \
+#define SETUP_CLASS_DESCRIPTION(name, ...)      \
+  static val name##__ = NULL;                   \
+                                                \
+  val name (void) {                             \
+    static atomic(bool) locked = false;         \
+                                                \
+    if (name##__ == NULL) {                     \
+      if (atomic_cas(&locked, false, true)) {   \
+        /* successfully acquired the lock */    \
+        val _desc = coal_new(__VA_ARGS__);      \
+                                                \
+        (void) atomic_cas(&name##__,            \
+                          (val) NULL,           \
+                          _desc);               \
+        locked = false;                         \
+      }                                         \
+                                                \
+      while (locked)                            \
+        continue;                               \
+    }                                           \
+                                                \
+    return name##__;                            \
   }
 
 #endif /* COAL_PRIVATE_OBJECT_ORIENTATION_H */
